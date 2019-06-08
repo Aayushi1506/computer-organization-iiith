5e4b14fae4e5bbf2754342e33aa35fa933b14609
Aayushi1506 <aayushi.ghadiya98@gmail.com>
2019-06-08 14:24:01 +0530

    FloatingPoint Exp html css and basic js

---
+++ src/lab/FloatingPointNumbers/JS/FloatingPoint.js
@@ -0,0 +1,36 @@
+
+
+  function chBackcolor() {
+     document.getElementById(nav1).style.background = "red";
+  }
+
+function myFunction1() {
+  var x =Number(document.getElementById("decno").value);
+  var y =Number(document.getElementById("bitno").value);
+  if(x=="" || y==""){
+  window.alert("enter the numbers first");
+    return false;
+  }
+  document.getElementById("lower_part").style.visibility="visible";
+
+
+  document.getElementById("input1").value = x + y ;
+  document.getElementById("input2").value = x - y;
+  document.getElementById("input3").value = x * y;
+  document.getElementById("input4").value = x/y;
+  document.getElementById("table1").rows[0].col[0].Value = "2";
+
+
+}
+
+function myFunction2() {
+    document.getElementById("lower_part").style.visibility="hidden";
+    document.getElementById("decno").value = "";
+      document.getElementById("bitno").value = "";
+  document.getElementById("input1").value = "";
+  document.getElementById("input2").value = "";
+  document.getElementById("input3").value = "";
+  document.getElementById("input4").value = "";
+}
+
+

b32a9c07379c3ceee35b5b73eafede20d78c046e
Aayushi1506 <aayushi.ghadiya98@gmail.com>
2019-06-22 19:03:28 +0530

    added functionality to buttons and linked the pages

--- SRIP/Codes/JS/FloatingPoint.js
+++ SRIP/Codes/JS/FloatingPoint.js
@@ -4,7 +4,7 @@
      document.getElementById(nav1).style.background = "red";
   }
 
-function myFunction1() {
+  function myFunction1() {
   var x =Number(document.getElementById("decno").value);
   var y =Number(document.getElementById("bitno").value);
   if(x=="" || y==""){
@@ -12,18 +12,14 @@ function myFunction1() {
     return false;
   }
   document.getElementById("lower_part").style.visibility="visible";
-
-
   document.getElementById("input1").value = x + y ;
   document.getElementById("input2").value = x - y;
   document.getElementById("input3").value = x * y;
   document.getElementById("input4").value = x/y;
   document.getElementById("table1").rows[0].col[0].Value = "2";
+  }
 
-
-}
-
-function myFunction2() {
+  function myFunction2() {
     document.getElementById("lower_part").style.visibility="hidden";
     document.getElementById("decno").value = "";
       document.getElementById("bitno").value = "";
@@ -31,6 +27,16 @@ function myFunction2() {
   document.getElementById("input2").value = "";
   document.getElementById("input3").value = "";
   document.getElementById("input4").value = "";
-}
+  }
+
+ function openTab(tabName) {
+  var i;
+  var x = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+  }
+
 
 

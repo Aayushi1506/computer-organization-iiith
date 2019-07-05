2f3566d718a8f58551a0b20932cc30fb1294d26b
Aayushi1506 <aayushi.ghadiya98@gmail.com>
2019-07-06 00:58:08 +0530

    added analyzer page

--- SRIP/Codes/JS/FloatingPoint.js
+++ SRIP/Codes/JS/FloatingPoint.js
@@ -1,35 +1,30 @@
 
 
-  function chBackcolor() {
-     document.getElementById(nav1).style.background = "red";
-  }
 
   function myFunction1() {
   var x =Number(document.getElementById("decno").value);
   var y =Number(document.getElementById("bitno").value);
-  if(x=="" || y==""){
-  window.alert("enter the numbers first");
-    return false;
+  if(x=="" && y==""){
+  x= 0;
+  y = 0;
+    
   }
-  document.getElementById("lower_part").style.visibility="visible";
-  document.getElementById("input1").value = x + y ;
-  document.getElementById("input2").value = x - y;
-  document.getElementById("input3").value = x * y;
-  document.getElementById("input4").value = x/y;
-  document.getElementById("table1").rows[0].col[0].Value = "2";
+  if(document.getElementById("lower_part").style.visibility=="hidden"){
+           document.getElementById("lower_part").style.visibility="visible";
+  }
+  else{
+    document.getElementById("lower_part").style.visibility="hidden";
+    }
+  
   }
 
   function myFunction2() {
-    document.getElementById("lower_part").style.visibility="hidden";
-    document.getElementById("decno").value = "";
-      document.getElementById("bitno").value = "";
-  document.getElementById("input1").value = "";
-  document.getElementById("input2").value = "";
-  document.getElementById("input3").value = "";
-  document.getElementById("input4").value = "";
+    
+  document.getElementById("lower_part").style.visibility="hidden"; 
+    
   }
 
- function openTab(tabName) {
+  function openTab(tabName) {
   var i;
   var x = document.getElementsByClassName("tab");
   for (i = 0; i < x.length; i++) {
